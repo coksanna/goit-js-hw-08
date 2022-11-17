945fb97422d947125c500c7a27de172ae6a12ab2
Oksana Kyryliuk <coksanna@gmail.com>
2022-11-17 20:51:13 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
+const iframeRef = document.querySelector('iframe');
 
+const player = new Player(iframeRef);
+player.on('timeupdate', throttle(setWatchingTime, 1000));
+const getContentOfLocalStorage = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(getContentOfLocalStorage.seconds || 0);
+
+function setWatchingTime(data) {
+    // console.log(data);
+    return localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    
+ }
